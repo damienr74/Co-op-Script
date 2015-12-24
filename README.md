9591bc3b0d9253f1125aff7e19321deeab827892
damienr74 <damienr74@gmail.com>
2015-12-24 17:04:16 -0400

    Initial commit, with script and README

---
+++ README.md
@@ -0,0 +1,4 @@
+Co-op script I use to display information on a status bar.
+
+If you have any inquiries on how to make this work for your
+favourite website, let me know and I *MIGHT* be able to help..
---
+++ generic_login.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# login to the website.
+
+curl -b cookie.txt -c cookie.txt --data \
+"action=hidden_field&Username=Username&password=password" \
+6 https://website.com/login.htm 2> /dev/null
+
+
+#go to the desired page.
+
+curl -b cookie.txt -c cookie.txt \
+https://website.com/information.htm \
+# the 2> /dev/null removes the downloading information
+-o postings.txt 2> /dev/null
+
+
+# if there are many divs, look for some text that identifies yours.
+# grep -nia will return the line number.
+
+start=`< all_info.html grep -nia \
+'div class="box boxContent" style="float: left;width: 195px;margin: 5px;' \
+|sed -n 's/:/ /p'| awk '{print $1}'` # pipe to sed to remove the trailing ':'
+# pipe to awk print only the number portion of it.
+
+
+# if there are many similar tags and you want the first, then tell it how far to look.
+
+end=`expr $start + 40`
+
+
+# use the $start,$end range to save to file and ignore remaining text.
+
+sed -n "$start,$end p" all_info.html >  subsection.html
+
+
+#remove the complete html file.
+rm all_info.html
+
+
+(
+	# remove the tags in front of the data you want to display.
+	# sorry for using sed | sed.. It worked so I didn't touch it..
+
+	count=`sed -n 's/[^0-9]*class="badge badge-info">//p' subsection.html | sed -r\
+	's/[0-9]+/& /' |awk '{print $1}'`
+
+
+	# print the data you want to display.
+
+	echo $count
+
+
+# format the output however you want it!
+) | awk '{print "\tFor My program: " $1 "\t| Applied to: " $2 "\t| Viewed: " $3}'\
+> /home/user/.file.txt # redirect to hidden file if you don't want your ~/ cluttured.
